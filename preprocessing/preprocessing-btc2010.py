b98eb7b7249efd9685740780e24823bc77eb8fa7
luciekaffee <www2018.550@gmail.com>
2017-11-21 13:30:46 +0100

    initial commit

---
+++ preprocessing/preprocessing-btc2010.py
@@ -0,0 +1,29 @@
+import os
+import gzip
+
+class Preprocessing_BTC_2010:
+    def __init__(folder, outfile):
+        self.folder = folder
+        self.out = outfile
+
+    def getData(self):
+        counter = 000
+        while True:
+            file = self.folder + 'btc-2010-chunk-' + str(counter).zfill(3) + '.gz'
+            if not os.path.exists(file):
+                break
+            print 'Processing file btc-2010-chunk-' + str(counter).zfill(3) + '.gz'
+            process_file(file)
+            counter += 1
+
+    def process_file(self, filepath):
+        file = gzip.open(filepath)
+        for line in file:
+            with open(self.out, 'a+') as out:
+                triple = line.split()[:-2]
+                s = triple[0]
+                p = triple[1]
+                o = ' '.join(triple[2:])
+                out.write(s + '\t' + p + '\t' + o + '\n')
+
+
