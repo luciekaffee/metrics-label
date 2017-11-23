11be0243f4bac0d1d79cceeb8d1dc271120aaf5d
luciekaffee <www2018.550@gmail.com>
2017-11-23 13:13:25 +0100

    limit to NIRS

--- processing/preprocessing_dumps.py
+++ processing/preprocessing_dumps.py
@@ -3,23 +3,21 @@ import gzip
 
 
 class Preprocessing_BTC_2010:
-    def __init__(self, folder, outfile):
+    def __init__(self, nirpath, folder, outfile):
         self.folder = folder
         self.out = outfile
+        self.nir = self.getNIRS(nirpath)
 
     def process_file(self, filepath):
-        try:
-            file = gzip.open(filepath)
-        except err:
-            print err
-            return
+        file = gzip.open(filepath)
         for line in file:
             with open(self.out, 'a+') as out:
                 triple = line.split()[:-2]
-                s = triple[0]
-                p = triple[1]
-                o = ' '.join(triple[2:])
-                out.write(s + '\t' + p + '\t' + o + '\n')
+                s = triple[0].strip()
+                p = triple[1].strip()
+                o = ' '.join(triple[2:]).strip()
+                if s in self.nir:
+                    out.write(s + '\t' + p + '\t' + o + '\n')
 
     def run(self):
         counter = 0
@@ -30,3 +28,10 @@ class Preprocessing_BTC_2010:
             print 'Processing file btc-2010-chunk-' + str(counter).zfill(3) + '.gz'
             self.process_file(file)
             counter += 1
+
+    def getNIRS(self, nirpath):
+        nirs = []
+        with gzip.open(nirpath) as infile:
+            for line in infile:
+                nirs.append(line.split()[0].strip())
+        return nirs
--- run.py
+++ run.py
@@ -2,12 +2,12 @@ from processing.preprocessing_dumps import *
 from processing.writer import *
 from evaluators.property import *
 
-pre_btc_2010 = Preprocessing_BTC_2010('../btc-2010/', 'data/btc-2010.csv')
+pre_btc_2010 = Preprocessing_BTC_2010( '../btc-2010/redirects.nx.gz', '../btc-2010/', 'data/1-btc-2010.csv')
 label_properties = LabelingProperties()
 writer = DictWriter()
 
 
-#pre_btc_2010.run()
-properties = label_properties.run('data/btc-2010.csv')
+pre_btc_2010.run()
+properties = label_properties.run('data/1-btc-2010.csv')
 properties =label_properties.limit(properties)
 writer.write('data/properties.csv', properties)
