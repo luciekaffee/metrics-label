69cc6dd68dbee4db06d8ecfa31fa819582c36cfe
luciekaffee <lucie.kaffee@gmail.com>
2018-04-12 14:48:04 +0100

    adjust Wikidata runfile

--- Preprocessing/convertData.py
+++ Preprocessing/convertData.py
@@ -1,6 +1,34 @@
 import gzip
 import os
 import hashlib
+import gzip
+
+
+class ConvertDataWikidata():
+    def __init__(self, infile, outfile):
+        self.infile = infile
+        self.outfile = outfile
+
+    def convert(self, line):
+        triple = line.strip().split(' ')
+        sub = triple[0]
+        if 'http://www.wikidata.org/entity/' not in sub:
+            return None
+        sub = sub.replace('http://www.wikidata.org/entity/', '')
+        pred = triple[1]
+        if len(triple) == 3 and 'http://www.wikidata.org/entity/' in triple[2]:
+            obj = triple[2].replace('http://www.wikidata.org/entity/', '')
+        else:
+            obj = " ".join(triple[2:])
+        return sub + '\t' + pred + '\t' + obj + '\n'
+
+    def run(self):
+        with gzip.open(self.outfile, 'wb') as outfile:
+            with gzip.open(self.infile) as infile:
+                for line in infile:
+                    text = self.convert(line)
+                    if text:
+                        outfile.write(text)
 
 
 class ConvertData():
@@ -41,7 +69,8 @@ class ConvertData():
 
     def run_2014(self):
         with gzip.open(self.outfile, 'wb') as out:
-            for i in range(1, 14):
+            # 14 folders with data (1 - 14)
+            for i in range(1, 15):
                 dir = self.directory + str(i).zfill(2) + '/'
                 for filename in os.listdir(dir):
                     if filename.startswith('data.nq') and filename.endswith('.gz'):
--- run-wikidata.py
+++ run-wikidata.py
@@ -3,15 +3,19 @@ from Metrics.labelMetrics import *
 from Metrics.labelingProperties import *
 
 datafile = '../latest-truthy.nt.gz'
+datasmall = 'latest-truthy-small.nt.gz'
 properties = 'Properties/properties-wikidata.csv'
 languages = '../results/Languages/wikidata.csv'
 monolanguages = '../results/Languages/wikidata-mono.csv'
 
-completness = Completeness(datafile, properties, seperator=' ')
-unam = Unambiguity(datafile, properties, seperator=' ')
-langs = Multilinguality(datafile, languages, seperator=' ')
-mono = MonolingualIsland(datafile, monolanguages, seperator=' ')
-labusa = LabelAndUsage(datafile, 'out-wikidata-labelandusage.csv', properties, seperator=' ')
+convert = ConvertDataWikidata(datafile, datasmall)
+convert.run()
+
+completness = Completeness(datasmall, properties)
+unam = Unambiguity(datasmall, properties)
+langs = Multilinguality(datasmall, languages)
+mono = MonolingualIsland(datasmall, monolanguages)
+labusa = LabelAndUsage(datasmall, 'out-wikidata-labelandusage.csv', properties)
 
 completness.run()
 unam.run()
